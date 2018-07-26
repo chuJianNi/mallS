868b02859d53b3ace74d144b1773b9b0c017b868
yangs <yys7586@qq.com>
2018-07-26 15:25:58 +0800

    add modify userinfo and userpassword...

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -24,9 +24,12 @@ class Index
 		{
 			$res=input('post.');
 			//dump($res);
+			trace('pas:'.$res["inputPassword"],'debug');
+			trace('pasAfterMd5:'.md5(md5($res["inputPassword"])),'debug');
 			$data=["user_id"=>$res["inputAccount"],
 				"user_name"=>$res["inputUsername"],
-				"password"=>$res["inputPassword"],
+				"password"=>md5(md5($res["inputPassword"])),
+				"mark"=>$res["inputMark"],
 				"valid"=>$res["optionsRadios"]];
 			$val=Db::table('goods_user')->insert($data);	
 			if($val === 1)
@@ -62,12 +65,27 @@ class Index
 		}
 	}
 
+	function checkPas()
+	{
+		if(request()->isPost())
+		{
+			trace('oldpasword: '.input('post.pas'),'debug');
+			trace('oldpaswordAfterMd5: '.md5(md5(input('post.pas'))),'debug');
+			$val=Db::table('goods_user')->where(['id'=>input('post.id'),'password'=>md5(md5(input('post.pas')))])->count();
+			if($val === 1){
+				return json(["info"=>"right"]);
+			}else{
+				return json(["info"=>"error"]);
+			}
+		}
+	}
+
 	function getuser()
 	{
 		if(request()->isPost())
 		{
 			$res=input('post.');
-			$val=Db::table('goods_user')->field('id,user_id,user_name,valid')->select();
+			$val=Db::table('goods_user')->field('id,user_id,user_name,mark,valid')->select();
 			$res['data']=$val;
 			return json($res);
 		}
@@ -79,6 +97,46 @@ class Index
 		{
 			$res=input('post.user_id');
 			$val=Db::table('goods_user')->delete($res);
+			if($val === 1){
+				return json(["info"=>"done"]);
+			}else{
+				return json(["info"=>"error"]);
+			}
+		}
+	}
+
+	function userinfo()
+	{
+		if(request()->isPost())
+		{
+			$res=input('post.id');
+			$val=Db::table('goods_user')->field('user_id,user_name,valid,mark')->where('id',$res)->find();
+			return json($val);
+		}
+	}
+
+	function edituser()
+	{
+		if(request()->isPost())
+		{
+			$res=input('post.');
+			$val=Db::table('goods_user')->where('id',$res['id'])->update(['mark'=>$res['inputMarkM'],'valid'=>$res['optionsRadiosM']]);
+			error_log('update goods_user ,field: mark valid.');
+			trace('update goods_user. field:mark valid.','debug');
+			trace($val,'debug');
+			if($val === 1){
+				return json(["info"=>"done"]);
+			}else{
+				return json(["info"=>"error"]);
+			}
+		}
+	}
+	function editpas()
+	{
+		if(request()->isPost())
+		{
+			$res=input('post.');
+			$val=Db::table('goods_user')->where('id',$res['id'])->update(['password'=>md5(md5($res['pas']))]);
 			if($val === 1){
 				return json(["info"=>"done"]);
 			}else{
