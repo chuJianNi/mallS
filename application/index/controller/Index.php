f77c17ec02b9303a5f5f332bd40c05450966a48c
yangs <yys7586@qq.com>
2018-08-08 19:01:27 +0800

    rewrite js funs

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -18,25 +18,35 @@ class Index
 		return view('goods');
 	}
 
-	function adduser()
+	function addUser()
 	{
 		if(request()->isPost())
 		{
-			$res=input('post.');
+			$res=(input('post.'));
+			$where=$res['where'];
+			$updates = $res['updates'];
+			$val=Db::table('goods_user')->where(['user_name'=>$where['username']])->count();
+			if($val === 1){
+				return json(["msg"=>"用户名被占用","info"=>"error"]);
+			}
+			$val=Db::table('goods_user')->where(['user_id'=>$where['self']])->count();
+			if($val === 1){
+				return json(["msg"=>"账号被占用","info"=>"error"]);
+			}
 			//dump($res);
-			trace('pas:'.$res["inputPassword"],'debug');
-			trace('pasAfterMd5:'.md5(md5($res["inputPassword"])),'debug');
-			$data=["user_id"=>$res["inputAccount"],
-				"user_name"=>$res["inputUsername"],
-				"password"=>md5(md5($res["inputPassword"])),
-				"mark"=>$res["inputMark"],
-				"valid"=>$res["optionsRadios"]];
+			trace('pas:'.$updates["inputPassword"],'debug');
+			trace('pasAfterMd5:'.md5(md5($updates["inputPassword"])),'debug');
+			$data=["user_id"=>$updates["self"],
+				"user_name"=>$updates["inputUsername"],
+				"password"=>md5(md5($updates["inputPassword"])),
+				"mark"=>$updates["inputMark"],
+				"valid"=>$updates["optionsRadios"]];
 			$val=Db::table('goods_user')->insert($data);	
 			if($val === 1)
 			{
-				return json(["info"=>"保存成功"]);
+				return json(["info"=>"ok","msg"=>"保存用户信息成功"]);
 			}
-			return json(["info"=>"保存失败"]);
+			return json(["info"=>"error","msg"=>"保存用户信息失败"]);
 		}
 	}
 
@@ -44,24 +54,32 @@ class Index
 	{
 		if(request()->isPost())
 		{
-			$val=Db::table('goods_user')->where(['user_name'=>input('post.user_name')])->count();
-			if($val === 1){
-				return json(["info"=>"have"]);
-			}else{
-				return json(["info"=>"no"]);
+			$res=input('post.');
+			$where=$res['where'];
+			$val=Db::table('goods_user')->where(['user_name'=>$where['self']])->count();
+			if($val === 0){
+				return json(["info"=>"ok","msg"=>""]);
 			}
+			if($val === 1){
+				return json(["info"=>"error","msg"=>"用户名已被使用"]);
+			}
+			return json(["info"=>"error","msg"=>"未知错误"]);
 		}
 	}
 	function checkUserid()
 	{
 		if(request()->isPost())
 		{
-			$val=Db::table('goods_user')->where(['user_id'=>input('post.user_id')])->count();
-			if($val === 1){
-				return json(["info"=>"have"]);
-			}else{
-				return json(["info"=>"no"]);
+			$res=input('post.');
+			$where=$res['where'];
+			$val=Db::table('goods_user')->where(['user_id'=>$where['self']])->count();
+			if($val === 0){
+				return json(["info"=>"ok","msg"=>""]);
 			}
+			if($val === 1){
+				return json(["info"=>"error","msg"=>"账号已被使用"]);
+			}
+			return json(["info"=>"error","msg"=>"未知错误"]);
 		}
 	}
 
@@ -70,35 +88,46 @@ class Index
 	{
 		if(request()->isPost())
 		{
-			//记录log
-			trace('oldpasword: '.input('post.pas'),'debug');
-			trace('oldpaswordAfterMd5: '.md5(md5(input('post.pas'))),'debug');
-			$val=Db::table('goods_user')->where(['id'=>input('post.id'),'password'=>md5(md5(input('post.pas')))])->count();
+			$res=input('post.');
+			$where=$res['where'];
+			$updates=$res['updates'];
+			//记录log sad
+			trace('oldpasword: '.$where['pwdOld'],'debug');
+			trace('oldpaswordAfterMd5: '.md5(md5($where['pwdOld'])),'debug');
+			$val=Db::table('goods_user')->where(['id'=>$where['id'],'password'=>md5(md5($where['pwdOld']))])->count();
 			if($val === 1){
-				$val=Db::table('goods_user')->where('id',input('post.id'))->update(['password'=>md5(md5(input('post.pasN')))]);
+				$val=Db::table('goods_user')->where('id',$where['id'])->update(['password'=>md5(md5($updates['self']))]);
 				if($val === 1){
-					return json(["info"=>"done"]);
-				}else{
-					return json(["info"=>"error"]);
+					return json(["info"=>"ok","msg"=>"修改密码成功"]);
 				}
+				if($val === 0){
+					return json(["info"=>"error","msg"=>"没有修改操作"]);
+				}
+				return json(["info"=>"error","msg"=>"未知错误"]);
 
-			}else{
-				return json(["info"=>"opw"]);
 			}
+			if($val === 0){
+				return json(["info"=>"error","msg"=>"原密码不正确"]);
+			}
+			return json(["info"=>"error","msg"=>"未知错误"]);
 		}
 
 	}
 
-	function checkpas()
+	function checkPas()
 	{
 		if(request()->isPost())
 		{
-			$val=Db::table('goods_user')->where(['id'=>input('post.id'),'password'=>md5(md5(input('post.pas')))])->count();
+			$res=input('post.');
+			$where=$res['where'];
+			$val=Db::table('goods_user')->where(['id'=>$where['id'],'password'=>md5(md5($where['self']))])->count();
 			if($val === 1){
-				return json(["info"=>"have"]);
-			}else{
-				return json(["info"=>"error"]);
+				return json(["info"=>"ok","msg"=>""]);
 			}
+			if($val === 0){
+				return json(["info"=>"error","msg"=>"原密码错误"]);
+			}
+			return json(["info"=>"error","msg"=>"未知错误"]);
 		}
 	}
 
@@ -114,19 +143,23 @@ class Index
 		}
 	}
 
-	function deluser()
+	function delUser()
 	{
 		if(request()->isPost())
 		{
 			trace('try delete user in  table: goods_user ...','debug');
-			$res=input('post.user_id');
-			$val=Db::table('goods_user')->delete($res);
+			$res=input('post.');
+			$where=$res['where'];
+			$val=Db::table('goods_user')->delete($where['user_id']);
 			if($val === 1){
-				trace('primary id:'.$res.' is deleted in table:goods_user','debug');
-				return json(["info"=>"done"]);
-			}else{
-				return json(["info"=>"error"]);
+				trace('primary id:'.$where['user_id'].' is deleted in table:goods_user','debug');
+				return json(["info"=>"ok","msg"=>"删除用户成功"]);
 			}
+			if($val === 0){
+				trace('primary id:'.$where['user_id'].' not exist in table:goods_user','debug');
+				return json(["info"=>"error","msg"=>"没有用户信息"]);
+			}
+			return json(["info"=>"error","msg"=>"未知错误"]);
 		}
 	}
 
@@ -140,20 +173,23 @@ class Index
 		}
 	}
 
-	function edituser()
+	function editUser()
 	{
 		if(request()->isPost())
 		{
 			$res=input('post.');
-			$val=Db::table('goods_user')->where('id',$res['id'])->update(['mark'=>$res['inputMarkM'],'valid'=>$res['optionsRadiosM']]);
-			error_log('update goods_user ,field: mark valid.');
+			$where=$res['where'];
+			$updates=$res['updates'];
+			$val=Db::table('goods_user')->where('id',$where['id'])->update(['mark'=>$updates['self'],'valid'=>$updates['optionsRadiosM']]);
 			trace('update goods_user. field:mark valid.','debug');
 			trace($val,'debug');
 			if($val === 1){
-				return json(["info"=>"done"]);
-			}else{
-				return json(["info"=>"error"]);
+				return json(["info"=>"ok","msg"=>"修改用户信息成功"]);
 			}
+			if($val === 0){
+				return json(["info"=>"error","msg"=>"没有修改操作"]);
+			}
+			return json(["info"=>"error","msg"=>"未知错误"]);
 		}
 	}
 	
